65467e06253946ebb71e5bbbd0929bd8b292d788
Daren Zammit <daren@hive.com.mt>
2017-08-16 08:24:50 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+Slider plugin based on ACF and Swiper.
+Can be used with or without front-end
---
+++ hex-hero-slider.php
@@ -0,0 +1,8 @@
+<?php
+/*
+Plugin Name: Hex Hero Slider
+Description: Slider plugin for pages, posts, archives and more.
+Version: 1.0
+Author: Daren Zammit
+Author URI: https://darenzammit.com/
+ */
