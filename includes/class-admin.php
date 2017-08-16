f5a37ebc066003b45922efb92fcfe92af9a220ca
Daren Zammit <daren@hive.com.mt>
2017-08-16 12:52:36 +0200

    Renamed class filename

--- includes/class-admin.php
+++ includes/class-admin.php
@@ -2,18 +2,22 @@
 
 class Hex_Hero_Slider_Admin {
 
+	/**
+	 * HexHeroSlider Constructor.
+	 */
 	function __construct() {
 
 		$this->add_options_page();
-		
+
 		add_filter('acf/load_field/name=hero_slider_display', [$this, "remove_default_option_from_settings"]);
-		
+
 	}
 
 	/**
 	 * Add Hero Options Page
 	 */
 	private function add_options_page() {
+
 		if (function_exists('acf_add_options_page')) {
 			acf_add_options_sub_page([
 				'page_title'  => 'Hero Slider Settings',
@@ -22,18 +26,22 @@ class Hex_Hero_Slider_Admin {
 				'parent_slug' => 'edit.php?post_type=hex_hero_slide',
 			]);
 		}
+
 	}
 
 	/**
 	 * Remove the Default option from the settings page
 	 */
 	public function remove_default_option_from_settings($field) {
+
 		if (is_admin() && $current_screen = get_current_screen()) {
 			if ('hex_hero_slide_page_settings' == $current_screen->id) {
 				unset($field['choices']['default']);
 			}
 		}
+
 		return $field;
+
 	}
 
 }
